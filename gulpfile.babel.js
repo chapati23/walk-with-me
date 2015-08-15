92c54515786df517a1dd4e14c517aefcbf4f4517
chapati <philip.paetz@me.com>
2015-08-15 14:51:08 +0200

    try force-adding CNAME to gh-pages branch so it doesnt get deleted on every deploy

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -162,7 +162,7 @@ gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
 
 gulp.task('deploy', ['build'], function() {
   return gulp.src('./dist/**/*')
-    .pipe($.ghPages());
+    .pipe($.ghPages({ force: true }));
 });
 
 gulp.task('default', ['clean'], () => {

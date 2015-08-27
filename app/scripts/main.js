6050449abfb2e11d142ed64575c491f6ecf780b1
chapati23 <philip.paetz@me.com>
2015-08-27 18:56:22 +0200

    ridiculous cookie

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,26 @@
 'use strict';
 
+(function() {
+  var passwordForm = document.getElementById('password-form');
+  var password = document.getElementById('password').value;
+
+  if(document.cookie.match(/access=granted/)){
+    passwordForm.style.display = 'none';
+  }
+
+})();
+
 function authenticate(event) {
   event.preventDefault();
   event.stopPropagation();
   var passwordForm = document.getElementById('password-form');
   var password = document.getElementById('password').value;
+
   if (password === 'r3fug33s-w3lc0m3') {
     passwordForm.style.display = 'none';
+    document.cookie="access=granted";
   } else {
     window.alert('Wrong Password');
   }
 }
+

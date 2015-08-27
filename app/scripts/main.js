11c9083324dcd29a572767720b6b03b93e588c0c
chapati23 <philip.paetz@me.com>
2015-08-27 18:40:46 +0200

    implemented most insecure authentication mechanism ever

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+function authenticate(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  var passwordForm = document.getElementById('password-form');
+  var password = document.getElementById('password').value;
+  if (password === 'r3fug33s-w3lc0m3') {
+    passwordForm.style.display = 'none';
+  } else {
+    window.alert('Wrong Password');
+  }
+}

6c5c38bf3e106175126eceb1f154b984a43a14f7
chapati23 <philip.paetz@me.com>
2015-10-12 16:58:23 +0200

    cleanup

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-shipit production deploy
--- run_as_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build --rm=true --no-cache=false -t wwm/wwm . && \
-docker stop wwm | xargs docker rm -fv && \
-docker run -tid -p 80:80 --name wwm wwm/wwm

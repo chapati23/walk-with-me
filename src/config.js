408ae3f669d75aafc82e591e66d7ca142d978795
chapati23 <philip.paetz@me.com>
2015-10-22 15:41:04 +0200

    new packages

--- src/config.js
+++ src/config.js
@@ -17,13 +17,18 @@ System.config({
   map: {
     "angular": "github:angular/bower-angular@1.4.7",
     "angular-animate": "github:angular/bower-angular-animate@1.4.7",
+    "angular-elastic": "npm:angular-elastic@2.5.0",
     "angular-es6": "npm:angular-es6@1.0.3",
     "angular-inview": "npm:angular-inview@1.5.0",
     "angular-sanitize": "github:angular/bower-angular-sanitize@1.4.7",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.15",
+    "angularfire": "github:firebase/angularfire@1.1.3",
+    "api-check": "npm:api-check@7.5.3",
     "babel": "npm:babel-core@5.8.25",
     "babel-runtime": "npm:babel-runtime@5.8.25",
     "core-js": "npm:core-js@1.2.1",
+    "firebase": "github:firebase/firebase-bower@2.3.1",
+    "ng-file-upload": "npm:ng-file-upload@9.0.19",
     "odometer": "bower:odometer@0.4.7",
     "github:angular-ui/ui-router@0.2.15": {
       "angular": "github:angular/bower-angular@1.4.7"
@@ -46,8 +51,14 @@ System.config({
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
+    "npm:angular-elastic@2.5.0": {
+      "angular": "npm:angular@1.4.7"
+    },
     "npm:angular-es6@1.0.3": {
       "path": "github:jspm/nodelibs-path@0.1.0"
+    },
+    "npm:angular@1.4.7": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
@@ -62,6 +73,9 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:ng-file-upload@9.0.19": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"

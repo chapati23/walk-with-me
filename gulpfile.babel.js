f7266249b0903fd2ad264f650c6644a3e63016e8
chapati23 <philip.paetz@me.com>
2015-12-30 19:00:00 +0100

    bump version before deploy

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,8 @@ const $ = gulpLoadPlugins();
 
 gulp.task('default',        $.shell.task(['npm start']));
 gulp.task('build',          $.shell.task(['npm run build -s']));
-gulp.task('deploy', ['build'], function() {
+gulp.task('bump-version',   $.shell.task(['npm version patch']));
+gulp.task('deploy', ['build', 'bump-version'], function() {
     return gulp.src('./dist/**/*')
     .pipe($.ghPagesCname({ cname: 'www.walkwithme.berlin' }));
 });
